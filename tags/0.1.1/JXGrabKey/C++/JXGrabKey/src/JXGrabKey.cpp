4cf9bf9acc4a202b98c76c9d363308ba61bbbd8a
subes <subes@7aed81b3-e467-4fe5-90e2-1a4c1332f389>
2009-01-01 19:17:36 +0000

    tag for release 0.1.1

---
+++ tags/0.1.1/JXGrabKey/C++/JXGrabKey/src/JXGrabKey.cpp
@@ -0,0 +1,366 @@
+/*	Copyright 2008  Edwin Stang (edwinstang@gmail.com), 
+ *
+ *  This file is part of JXGrabKey.
+ *
+ *  JXGrabKey is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  JXGrabKey is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with JXGrabKey.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define SLEEP_TIME 100
+
+#include "JXGrabKey.h"
+#include <X11/Xlib.h>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+struct KeyStruct {
+    int id;
+    KeyCode key;
+    Mask mask;
+};
+
+Display *dpy;
+bool debug = false;
+bool isListening = false;
+bool errorInListen = false;
+bool doListen = true;
+vector<KeyStruct> keys;
+
+JNIEXPORT void JNICALL Java_jxgrabkey_JXGrabKey_clean
+  (JNIEnv *_env, jobject _obj){
+    if(debug){
+        ostringstream sout;
+        sout << "++ clean()";
+        printToDebugCallback(_env, sout.str().c_str());
+    }
+    while(!isListening && !errorInListen){
+        if(debug){
+            ostringstream sout;
+            sout << "clean() - sleeping " << std::dec << SLEEP_TIME << " ms for listen() to be ready";
+            printToDebugCallback(_env, sout.str().c_str());
+        }
+        usleep(SLEEP_TIME*1000);
+    }
+    if(errorInListen){
+        if(debug){
+            ostringstream sout;
+            sout << "clean() - aborting because of error in listen()";
+            printToDebugCallback(_env, sout.str().c_str());
+        }
+        return;
+    }
+
+    for(int i = 0; i < keys.size(); i++){
+        Java_jxgrabkey_JXGrabKey_unregisterHotKey(_env, _obj, keys.at(i).id);
+    }
+
+    if(debug){
+        ostringstream sout;
+        sout << "clean() - stopping listen loop";
+        printToDebugCallback(_env, sout.str().c_str());
+    }
+
+    doListen = false;
+    
+    if(debug){
+        ostringstream sout;
+        sout << "-- clean()";
+        printToDebugCallback(_env, sout.str().c_str());
+    }
+}
+
+JNIEXPORT void JNICALL Java_jxgrabkey_JXGrabKey_registerHotkey__III
+  (JNIEnv *_env, jobject _obj, jint _id, jint _mask, jint _key){
+    if(debug){
+        ostringstream sout;
+        sout << "++ registerHotkey(" << std::dec << _id << ", 0x" << std::hex << _mask << ", 0x" << std::hex << _key << ")";
+        printToDebugCallback(_env, sout.str().c_str());
+    }
+    
+    while(!isListening && !errorInListen){
+        if(debug){
+            ostringstream sout;
+            sout << "registerHotkey() - sleeping " << std::dec << SLEEP_TIME << " ms for listen() to be ready";
+            printToDebugCallback(_env, sout.str().c_str());
+        }
+        usleep(SLEEP_TIME*1000);
+    }
+    
+    if(errorInListen){
+        if(debug){
+            ostringstream sout;
+            sout << "registerHotkey() - aborting because of error in listen(): errorInListen = " << std::dec << errorInListen;
+            printToDebugCallback(_env, sout.str().c_str());
+        }
+        return;
+    }
+
+    for(int i = 0; i < keys.size(); i++){
+        if(keys[i].id == _id){
+            Java_jxgrabkey_JXGrabKey_unregisterHotKey(_env, _obj, _id);
+            break;
+        }
+    }
+    
+    struct KeyStruct key;
+    key.id = _id;
+    
+    key.key = XKeysymToKeycode(dpy, _key);
+    key.mask = _mask;
+
+    keys.push_back(key);
+    
+    if(debug){
+        ostringstream sout;
+
+        sout << "registerHotkey() - converted x11Keysym '" <<  XKeysymToString(_key) << "' (0x" << std::hex << _key << ") to x11Keycode (0x" << std::hex << (int)key.key << ")";
+
+        sout << "registerHotkey() - found in x11Mask (0x" << std::hex << key.mask << "): ";
+
+        if(key.mask & ShiftMask){
+            sout << "'Shift' ";
+        }
+        if(key.mask & LockMask){
+            sout << "'Lock' ";
+        }
+        if(key.mask & ControlMask){
+            sout << "'Control' ";
+        }
+        if(key.mask & Mod1Mask){
+            sout << "'Mod1' ";
+        }
+        if(key.mask & Mod2Mask){
+            sout << "'Mod2' ";
+        }
+        if(key.mask & Mod3Mask){
+            sout << "'Mod3' ";
+        }
+        if(key.mask & Mod4Mask){
+            sout << "'Mod4' ";
+        }
+        if(key.mask & Mod5Mask){
+            sout << "'Mod5' ";
+        }
+
+        printToDebugCallback(_env, sout.str().c_str());
+    }
+
+    for (int screen = 0; screen < ScreenCount(dpy); screen++){
+        int ret = XGrabKey(dpy, key.key, key.mask, RootWindow(dpy, screen), True, GrabModeAsync, GrabModeAsync);
+        if(debug){
+            ostringstream sout;
+            sout << "registerHotkey() - XGrabKey() returned '" << getErrorString(ret) << "' (" << std::dec << ret << ")";
+            printToDebugCallback(_env, sout.str().c_str());
+        }
+    }
+    
+    if(debug){
+        ostringstream sout;
+        sout << "-- registerHotkey()";
+        printToDebugCallback(_env, sout.str().c_str());
+    }
+}
+
+JNIEXPORT void JNICALL Java_jxgrabkey_JXGrabKey_unregisterHotKey
+  (JNIEnv *_env, jobject _obj, jint _id){
+    if(debug){
+        ostringstream sout;
+        sout << "++ unregisterHotkey(" << std::dec << _id << ")";
+        printToDebugCallback(_env, sout.str().c_str());
+    }
+    
+    while(!isListening && !errorInListen){
+        if(debug){
+            ostringstream sout;
+            sout << "unregisterHotkey() - sleeping " << std::dec << SLEEP_TIME << " ms for listen() to be ready";
+            printToDebugCallback(_env, sout.str().c_str());
+        }
+        usleep(SLEEP_TIME*1000);
+    }
+    if(errorInListen){
+        if(debug){
+            ostringstream sout;
+            sout << "unregisterHotkey() - aborting because of error in listen()";
+            printToDebugCallback(_env, sout.str().c_str());
+        }
+        return;
+    }
+    
+    for(int i = 0; i < keys.size(); i++){
+        if(keys.at(i).id == _id){
+            for (int screen = 0; screen < ScreenCount(dpy); screen++){
+                int ret = XUngrabKey(dpy, keys.at(i).key, keys.at(i).mask, RootWindow(dpy, screen));
+                if(debug){
+                    ostringstream sout;
+                    sout << "unregisterHotkey() - XUngrabKey() returned '" << getErrorString(ret) << "' (" << std::dec << ret << ")";
+                    printToDebugCallback(_env, sout.str().c_str());
+                }
+            }
+            keys.erase(keys.begin()+i);
+            break;
+        }
+    }
+    
+    if(debug){
+        ostringstream sout;
+        sout << "-- unregisterHotkey()";
+        printToDebugCallback(_env, sout.str().c_str());
+    }
+}
+
+JNIEXPORT void JNICALL Java_jxgrabkey_JXGrabKey_listen
+  (JNIEnv *_env, jobject _obj){ 
+    
+    if(debug){
+        ostringstream sout;
+        sout << "++ listen()";
+        printToDebugCallback(_env, sout.str().c_str());
+    }
+
+    if(isListening){
+        if(debug){
+            ostringstream sout;
+            sout << "listen() - WARNING: already listening, aborting";
+            printToDebugCallback(_env, sout.str().c_str());
+        }
+        return;
+    }
+    
+    jclass cls = _env->FindClass("jxgrabkey/JXGrabKey");
+    if(cls == NULL){
+        if(debug){
+            ostringstream sout;
+            sout << "listen() - ERROR: cannot find class jxgrabkey.JXGrabKey";
+            printToDebugCallback(_env, sout.str().c_str());
+        }
+        errorInListen = true;
+        return;
+    }
+    
+    jmethodID mid = _env->GetStaticMethodID(cls, "fireKeyEvent", "(I)V" );
+    if(mid == NULL){
+        if(debug){
+            ostringstream sout;
+            sout << "listen() - ERROR: cannot find method fireKeyEvent(int)";
+            printToDebugCallback(_env, sout.str().c_str());
+        }
+        errorInListen = true;
+        return;
+    }
+
+    dpy = XOpenDisplay(NULL);
+    
+    if(!dpy){
+        if(debug){
+            ostringstream sout;
+            sout << "listen() - ERROR: cannot open display " << XDisplayName(NULL);
+            printToDebugCallback(_env, sout.str().c_str());
+        }
+        errorInListen = true;
+        return;
+    }
+
+    doListen = true;
+    isListening = true;
+
+    XEvent ev;
+    
+    if(debug){
+        ostringstream sout;
+        sout << "listen() - finished initialization on display " << XDisplayName(NULL);
+        printToDebugCallback(_env, sout.str().c_str());
+    }
+
+    while(doListen){
+
+        while(!XPending(dpy) && doListen){ //Don't block on XNextEvent(), this breaks XGrabKey()!
+            usleep(SLEEP_TIME*1000);
+        }
+
+        if(doListen){
+            XNextEvent(dpy, &ev);
+            if(debug){
+                ostringstream sout;
+                switch(ev.type){
+                    case KeyPress:
+                        sout << "listen() - received: type = KeyPress; x11Keycode = '" << XKeysymToString(XKeycodeToKeysym(dpy, ev.xkey.keycode, 0)) << "' (0x" << std::hex << ev.xkey.keycode << "); x11Mask = 0x" << std::hex << ev.xkey.state;
+                        break;
+                    case KeyRelease:
+                        sout << "listen() - received: type = KeyRelease; x11Keycode = '" << XKeysymToString(XKeycodeToKeysym(dpy, ev.xkey.keycode, 0)) << "' (0x" << std::hex << ev.xkey.keycode << "); x11Mask = 0x" << std::hex << ev.xkey.state;
+                        break;
+                    default:
+                        sout << "listen() - received unknown XEvent: type = " << std::dec << ev.type;
+                }
+                printToDebugCallback(_env, sout.str().c_str());
+            }
+            if(ev.type == KeyPress){
+                for(int i = 0; i < keys.size(); i++){
+                    if(ev.xkey.keycode == keys.at(i).key && ev.xkey.state == keys.at(i).mask){
+                        if(debug){
+                            ostringstream sout;
+                            sout << "listen() - found suitable key registration: id = " << std::dec << keys.at(i).id;
+                            printToDebugCallback(_env, sout.str().c_str());
+                        }
+                        _env->CallStaticVoidMethod(cls, mid, keys.at(i).id);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    isListening = false;
+    
+    if(debug){
+        ostringstream sout;
+        sout << "-- listen()";
+        printToDebugCallback(_env, sout.str().c_str());
+    }
+}
+
+JNIEXPORT void JNICALL Java_jxgrabkey_JXGrabKey_setDebug
+  (JNIEnv *_env, jobject _obj, jboolean _debug){
+    debug = _debug;
+}
+
+const char* getErrorString(int errorCode){
+    switch(errorCode){
+        case BadAccess:
+            return "BadAccess";
+        case BadValue:
+            return "BadValue";
+        case BadWindow:
+            return "BadWindow";
+        case 1:
+            return "Success";
+        default:
+            return "Unknown";
+    }
+}
+
+void printToDebugCallback(JNIEnv *_env, const char* message){
+    if(debug){
+        static jclass cls = _env->FindClass("jxgrabkey/JXGrabKey");
+        static jmethodID mid = _env->GetStaticMethodID(cls, "debugCallback", "(Ljava/lang/String;)V" );
+
+        if(mid != NULL){
+            _env->CallStaticVoidMethod(cls, mid, _env->NewStringUTF(message));
+        }else{
+            cout << "JAVA DEBUG CALLBACK NOT FOUND - " << message << endl;
+            fflush(stdout);
+        }
+    }
+}
---
+++ tags/0.1.1/JXGrabKey/Java/JXGrabKey/src/jxgrabkey/JXGrabKey.java
@@ -0,0 +1,178 @@
+/*	Copyright 2008  Edwin Stang (edwinstang@gmail.com),
+ *
+ *  This file is part of JXGrabKey.
+ *
+ *  JXGrabKey is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  JXGrabKey is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with JXGrabKey.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package jxgrabkey;
+
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class JXGrabKey {
+
+    private static boolean debug = false;
+
+    public static final int X11_SHIFT_MASK = 1 << 0;
+    public static final int X11_LOCK_MASK = 1 << 1;
+    public static final int X11_CONTROL_MASK = 1 << 2;
+    public static final int X11_MOD1_MASK = 1 << 3;
+    public static final int X11_MOD2_MASK = 1 << 4;
+    public static final int X11_MOD3_MASK = 1 << 5;
+    public static final int X11_MOD4_MASK = 1 << 6;
+    public static final int X11_MOD5_MASK = 1 << 7;
+
+    private static JXGrabKey instance;
+    private static Thread thread;
+    private static ArrayList<HotkeyListener> listeners = new ArrayList<HotkeyListener>();
+
+    private JXGrabKey() {
+        thread = new Thread(){
+            @Override
+            public void run() {
+                listen();
+            }
+        };
+        thread.start();
+        try{
+            Thread.sleep(100); //block a bit, so listen can init everything
+        }catch(InterruptedException e){}
+    }
+
+    public static JXGrabKey getInstance(){
+        if(instance == null){
+            debugCallback("getInstance() - instance null, initializing");
+            instance = new JXGrabKey();
+        }
+        return instance;
+    }
+
+    public void addHotkeyListener(HotkeyListener listener){
+        if(listener == null){
+            throw new IllegalArgumentException("listener must not be null");
+        }
+        JXGrabKey.listeners.add(listener);
+    }
+
+    public void removeHotkeyListener(HotkeyListener listener){
+        if(listener == null){
+            throw new IllegalArgumentException("listener must not be null");
+        }
+        JXGrabKey.listeners.remove(listener);
+    }
+
+    public void cleanUp(){
+        debugCallback("++ cleanUp()");
+        clean();
+        if(listeners.size() > 0){
+            if(thread.isAlive()){
+                debugCallback("cleanUp() - waiting for listen loop to stop");
+                while(thread.isAlive()){
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException ex) {}
+                }
+                debugCallback("cleanUp() - listen loop stopped, setting instance to null");
+                instance = null; //next time getInstance is called, reinitialize JXGrabKey
+            }
+            debugCallback("cleanUp() - removing all listeners");
+            listeners.clear();
+        }
+        debugCallback("-- cleanUp()");
+    }
+
+    private native void clean();
+
+    public native void registerHotkey(int id, int mask, int key);
+
+    public void registerAWTHotkey(int id, int mask, int key){
+
+        debugCallback("++ registerAWTHotkey()");
+
+        int x11Mask = 0;
+
+        if ((mask & InputEvent.SHIFT_MASK) != 0) {
+            x11Mask |= X11_SHIFT_MASK;
+        }
+        if ((mask & InputEvent.ALT_MASK) != 0) {
+            x11Mask |= X11_MOD1_MASK;
+        }
+        if ((mask & InputEvent.CTRL_MASK) != 0) {
+            x11Mask |= X11_CONTROL_MASK;
+        }
+        if ((mask & InputEvent.META_MASK) != 0) {
+            x11Mask |= X11_MOD2_MASK;
+        }
+        if ((mask & InputEvent.ALT_GRAPH_MASK) != 0) {
+            x11Mask |= X11_MOD5_MASK;
+        }
+
+        int keysym = X11KeysymDefinitions.AWTToX11Keysym(key);
+
+        debugCallback("registerAWTHotkey() - converted AWTKeycode '"+KeyEvent.getKeyText(key)+"' (0x"+Integer.toHexString(key)+") to x11Keysym 0x"+Integer.toHexString(keysym));
+
+        debugCallback("registerAWTHotkey() - converted AWTMask '"+KeyEvent.getKeyModifiersText(mask)+"' (0x"+Integer.toHexString(mask)+") to x11Mask 0x"+Integer.toHexString(x11Mask));
+
+        registerHotkey(id, x11Mask, keysym);
+
+        debugCallback("-- registerAWTHotkey()");
+    }
+
+    public native void unregisterHotKey(int id);
+
+    public native void listen();
+
+    private static native void setDebug(boolean debug);
+
+    public static void setDebugOutput(boolean enabled){
+        debug = enabled;
+        setDebug(enabled);
+    }
+
+    public static void fireKeyEvent(int id){
+        for(int i = 0; i < listeners.size(); i++){
+            listeners.get(i).onHotkey(id);
+        }
+    }
+
+    public static void debugCallback(String debugmessage){
+        if(debug){
+            debugmessage.trim();
+            if(debugmessage.charAt(debugmessage.length()-1) != '\n'){
+                debugmessage += "\n";
+            }else{
+                while(debugmessage.endsWith("\n\n")){
+                    debugmessage = debugmessage.substring(0, debugmessage.length()-1);
+                }
+            }
+
+            boolean found = false;
+            for(HotkeyListener l : listeners){
+                if(l instanceof HotkeyListenerDebugEnabled){
+                    ((HotkeyListenerDebugEnabled)l).debugCallback(debugmessage);
+                    found = true;
+                }
+            }
+
+            if(found == false){
+                System.out.print(debugmessage);
+            }
+        }
+    }
+
+}
